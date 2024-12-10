f3fe8a581ce508c72540d2963f86069c6163f326
mason z <s028214@students.lmsd.org>
2024-12-10 15:12:04 -0500

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Mason Zhu (student LM) on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
